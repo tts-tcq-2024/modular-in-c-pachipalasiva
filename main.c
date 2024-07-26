c3e245c48f8ccac63134165deacfba1de8fd9f96
pachipalasiva <120707070+pachipalasiva@users.noreply.github.com>
2024-07-26 12:14:08 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
+#include "color.h"
+
 int main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
 
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
+
+    PrintColorCodingManual();
 
     return 0;
 }
