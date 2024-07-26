cb4013bdbdb1c311ae536c1ffa8cc3bad6d29f71
pachipalasiva <120707070+pachipalasiva@users.noreply.github.com>
2024-07-26 11:36:48 +0530

    test.c

---
+++ test.c
@@ -0,0 +1,9 @@
+int main() {
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
+
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
+
+    return 0;
+}
