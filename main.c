fdaa8e15f2d1117fd2ae71279ff8341334f3d7e2
pachipalasiva <120707070+pachipalasiva@users.noreply.github.com>
2024-07-26 11:37:08 +0530

    Update main.c

--- main.c
+++ main.c
@@ -19,13 +19,3 @@ void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expected
     printf("Got pair number %d\n", pairNumber);
     assert(pairNumber == expectedPairNumber);
 }
-
-int main() {
-    testNumberToPair(4, WHITE, BROWN);
-    testNumberToPair(5, WHITE, SLATE);
-
-    testPairToNumber(BLACK, ORANGE, 12);
-    testPairToNumber(VIOLET, SLATE, 25);
-
-    return 0;
-}
